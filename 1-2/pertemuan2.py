a4c4c4a91ce275797198ecedc2ff7ded19fd1e3e
yusriyanti <yusriyanti.inf18@student.unu-jogja.ac.id>
2021-10-05 10:57:28 +0700

    Mengerjakan tugas Tipe Data pada Python

---
+++ 1-2/pertemuan2.py
@@ -0,0 +1,57 @@
+#eksplore type data pythone
+
+#type data string
+x="salam pergerakan"
+print(type(x))
+
+#type data integer
+y=20
+print(type(y))
+
+#type data float
+a=19.3
+print(type(a))
+
+#type data complex
+d = 5j
+print(type(d))
+
+#type data list
+i=["pete", "jengkol", "cempedak"]
+print(type(i))
+
+#type data tuple
+n=("pete", "jengkol", "cempedak")
+print(type(n))
+
+#type data range
+m=range(2018)
+print(type(m))
+
+#type data dict
+u={"name" : "adin", "age" : "23"}
+print(type(u))
+
+#type data set
+h={"jeruk", "anggur", "gedang"}
+print(type(h))
+
+#type data frozenset
+a=frozenset({"dzikir", "fikir", "amal sholeh"})
+print(type(a))
+
+#type data bool
+m=True
+print(type(m))
+
+#type data bytes
+m=b"sayang"
+print(type(m))
+
+#type data bytearray
+a=bytearray(22)
+print(type(a))
+
+#type data memoryview
+d=memoryview(bytes(22))
+print(type(d))
